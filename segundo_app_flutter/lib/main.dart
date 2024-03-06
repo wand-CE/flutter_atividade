36e347edb7c5db3a606dd8494b92a81a1e9121fd
wand-CE <wanderson14117@gmail.com>
2024-03-05 21:02:00 -0400

    changed assetImage to NetworkImage

--- segundo_app_flutter/lib/main.dart
+++ segundo_app_flutter/lib/main.dart
@@ -21,11 +21,27 @@ class SegundoApp extends StatefulWidget {
   const SegundoApp({super.key, required this.title});
   final title;
 
+
   @override
   State<SegundoApp> createState() => _SegundoAppState();
 }
 
 class _SegundoAppState extends State<SegundoApp> {
+  static List<String> landscapes = [
+    'https://www.ignant.com/wp-content/uploads/2016/04/lukegram_ontheroad-61.jpg',
+    'https://www.ignant.com/wp-content/uploads/2016/04/lukegram_ontheroad-62.jpg',
+    'https://www.ignant.com/wp-content/uploads/2016/04/lukegram_ontheroad-77.jpg',
+    'https://www.ignant.com/wp-content/uploads/2016/04/lukegram_ontheroad-125good.jpg',
+  ];
+
+  int _counter = 0;
+
+  void changeImage(){
+    setState(() {
+      _counter = (_counter == (landscapes.length - 1)) ? 0 : _counter+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,14 +51,14 @@ class _SegundoAppState extends State<SegundoApp> {
         title: Text(widget.title, style: TextStyle(fontSize: 25)),
         toolbarHeight: 100,
       ),
-      body: Image.asset(
-        'images/paisagem.jpg',
+      body: Image.network(
+        landscapes[_counter],
         fit: BoxFit.fill,
         height: 1000,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {print("Carregando")},
-        tooltip: 'pressionar',
+        onPressed: () => changeImage(),
+        tooltip: 'Trocar paisagem',
         child: const Icon(
           Icons.restore,
           color: Colors.deepOrange,
